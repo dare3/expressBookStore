36f82d55ba84d253e3586ec126d5be7f7911ef40
Dare3 <enitan_segun@yahoo.co.uk>
2024-09-07 17:28:16 -0400

    update code

--- db.js
+++ db.js
@@ -1,38 +1,14 @@
+/** Database config for database. */
+
+
 const { Client } = require("pg");
-const { DB_URI } = require("./config");
+const {DB_URI} = require("./config");
 
 let db = new Client({
-  connectionString: DB_URI,
+  connectionString: DB_URI
 });
 
-async function connectToDatabase() {
-  try {
-    await db.connect();
-    console.log("Successfully connected to the database.");
-  } catch (err) {
-    console.error("Database connection error:", err.stack);
-  }
-}
+db.connect();
 
-async function disconnectFromDatabase() {
-  try {
-    await db.end();
-    console.log("Database connection closed.");
-  } catch (err) {
-    console.error("Error closing database connection:", err.stack);
-  }
-}
-
-process.on('SIGINT', async () => {
-  await disconnectFromDatabase();
-  process.exit(0);
-});
-
-process.on('SIGTERM', async () => {
-  await disconnectFromDatabase();
-  process.exit(0);
-});
-
-connectToDatabase();
 
 module.exports = db;
